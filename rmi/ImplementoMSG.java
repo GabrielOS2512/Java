13d1befdf7edadbd3277e146b070840a4a1a51c7
Gabriel Oliveira Silva <gabrieloliveirasilva2512@gmail.com>
2021-02-21 22:13:26 -0300

    Update ImplementoMSG.java

--- rmi/ImplementoMSG.java
+++ rmi/ImplementoMSG.java
@@ -1,12 +1,12 @@
 import java.rmi.RemoteException;
 
-//Implementação da Mensagem
+//ImplementaÃ§Ã£o da Mensagem
 public class ImplementoMSG implements Mensagem {  
 
 	
 	public void printMsg(String nome) {  
 		System.out.println("|----- "+nome+" Finalizou -----|");
-		System.out.println("Trabalho  de Sistemas  Distribuídos – Professora  Carla  Lara");  
+		System.out.println("Trabalho  de Sistemas  DistribuÃ­dos");  
 	}
 	public void mensagem(String nome,String a) {  
 		   System.out.println(nome +" : " + a);  
